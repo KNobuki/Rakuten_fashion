364ec48b704a0a50a662a2262c968d55aa1fce73
yuka1324 <yu_ka092701@me.com>
2019-09-07 00:24:16 +0900

    no message

--- rakuten_back/api_test.php
+++ rakuten_back/api_test.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang='ja'>
 <head>
-<meta charset="utf-8">
+<title>楽天商品検索API テスト</title>
+<meta charset='utf-8'>
 <meta name="author" content="Rakuten Fashion Dev Team">
 <title>Rakuten Fashion</title>
 <link href="images/favicon.png" rel="icon">
@@ -13,12 +14,14 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js"></script>
 <script type="text/javascript" src="js/slick/slick.min.js"></script>
 <script type="text/javascript" src="js/main.js"></script>
-</head>
 
+</head>
 <body>
 <?php
     $i = 1; $args = [];
     $rakuten_relust = getRakutenResult('diesel  靴',5000); // キーワードと最低価格を指定
+    
+    
     foreach ( (array)$rakuten_relust as $item) :
     $test1 = explode("/",$rakuten_relust[$i-1]['url']);
     $key = in_array($test1[count($test1)-2], $args);
@@ -28,44 +31,29 @@
     }
     array_push($args, $test1[count($test1)-2]);
     ?>
-
-    <ul class="slider">
-    <?php
-        $imgCnt = 0;
-        foreach($item['ImageUrls'] as $images){
-    ?>
-        <li><a href=""><img src="<?php echo $item['ImageUrls'][$imgCnt]->imageUrl; ?>" alt=<?php echo 'image'.$imgCnt?>></a></li>
-    <?php
-        $imgCnt++;
-        }
-    ?>
-    </ul>
-    <ul class="thumb">
-    <?php
-        $imgCnt = 0;
-        foreach($item['ImageUrls'] as $images){
-    ?>
-        <li><a href="#"><img src="<?php echo $item['ImageUrls'][$imgCnt]->imageUrl; ?>" alt=<?php echo 'image'.$imgCnt?>></a></li>
+<ul class="slider">
 <?php
-        $imgCnt++;
-        }
-    ?>
-        <?php
-        for($i=count($item['ImageUrls']); $i<=4; $i++){
-    ?>
-        <li></li>
+    for($imgCnt = 0; $imgCnt < count($item['ImageUrls']); $imgCnt++){
+        ?>
+<li><a href=""><img src="<?php echo $item['ImageUrls'][$imgCnt]->imageUrl; ?>" alt=<?php echo 'image'.$imgCnt?>></a></li>
 <?php
-        }
+    }
     ?>
-    </ul>
-
-    <div style='margin-bottom: 20px; padding: 30px; border: 1px solid #000; overflow:hidden;'>
-<div style='float: left;'><img src='<?php echo $item['img']; ?>'></div>
-<div style='float: left; padding: 20px;'>
-
+</ul>
+<ul class="thumb">
+<?php
+    for($imgCnt = 0; $imgCnt < count($item['ImageUrls']); $imgCnt++){
+        ?>
+<li><a href="#"><img src="<?php echo $item['ImageUrls'][$imgCnt]->imageUrl; ?>" alt=<?php echo 'image'.$imgCnt?>></a></li>
+<?php
+    }
+    ?>
+</ul>
 <div><?php echo $item['name']; ?></div>
 <div><a href='<?php echo $item['url']; ?>' target="_blank"><?php echo $item['url']; ?></a></div>
 <div><?php echo $item['price']; ?>円</div>
+<div><?php echo $i; ?></div>
+
 <div><?php
     foreach((array)$item['tagId'] as $number){
         for($j = 1000873; $j <= 1000887; $j++){
@@ -99,10 +87,8 @@
         $params['sort'] = urlencode_rfc3986('+itemPrice'); // ソートの方法。※文字コードは UTF-8
         $params['minPrice'] = $min_price; // 最低価格
         $params['shopcode'] = 'kbf-rba'; //RBAのデータのみ取得
-        $params['hits'] = 30; //RBAのデータのみ取得
-        
-        
-        
+        $params['hits'] = 30;
+        $params['page'] = 1;
         $canonical_string='';
         
         foreach($params as $k => $v) {
@@ -148,4 +134,3 @@
     function urlencode_rfc3986($str) {
         return str_replace('%7E', '~', rawurlencode($str));
     }?>
-
